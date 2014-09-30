d1d255cbc2edcc9077a1a95e394cca9606425e2c
Ahmad Musaffa <musaffa_csemm@yahoo.com>
2014-10-01 03:37:02 +0600

    search strategy

--- lib/polymorphic_constraints/connection_adapters/postgresql_adapter.rb
+++ lib/polymorphic_constraints/connection_adapters/postgresql_adapter.rb
@@ -11,7 +11,8 @@ module PolymorphicConstraints
       end
 
       def add_polymorphic_constraints(relation, associated_table, options = {})
-        polymorphic_models = options.fetch(:polymorphic_models) { get_polymorphic_models(relation) }
+        search_strategy = options.fetch(:search_strategy, :active_record_descendants)
+        polymorphic_models = options.fetch(:polymorphic_models) { get_polymorphic_models(relation, search_strategy) }
         statements = []
         statements << generate_upsert_constraints(relation, associated_table, polymorphic_models)
         statements << generate_delete_constraints(relation, associated_table, polymorphic_models)
@@ -26,12 +27,28 @@ module PolymorphicConstraints
 
       private
 
-      def get_polymorphic_models(relation)
+      def get_polymorphic_models(relation, search_strategy)
+        search_strategy == :models_directory ? get_models_from_directory(relation) : get_active_record_descendents(relation)
+      end
+
+      def get_models_from_directory(relation)
+        models = Dir["#{Rails.root}/app/models/**/*.rb"].map { |f| File.basename(f, '.*').camelize.constantize }
+                                                        .select { |klass| klass.ancestors.include?(ActiveRecord::Base) }
+        models.select do |klass|
+          model_contains_relation?(klass, relation)
+        end
+      end
+
+      def get_active_record_descendents(relation)
         Rails.application.eager_load!
         ActiveRecord::Base.descendants.select do |klass|
-          associations = klass.reflect_on_all_associations
-          associations.map{ |r| r.options[:as] }.include?(relation.to_sym)
+          model_contains_relation?(klass, relation)
         end
+      end
+
+      def model_contains_relation?(model_class, relation)
+        associations = model_class.reflect_on_all_associations
+        associations.map{ |r| r.options[:as] }.include?(relation.to_sym)
       end
 
       def generate_upsert_constraints(relation, associated_table, polymorphic_models)
--- spec/lib/polymorphic_constraints/connection_adapters/postgresql_adapter_spec.rb
+++ spec/lib/polymorphic_constraints/connection_adapters/postgresql_adapter_spec.rb
@@ -3,6 +3,10 @@ require 'polymorphic_constraints/connection_adapters/postgresql_adapter'
 
 describe PolymorphicConstraints::ConnectionAdapters::PostgreSQLAdapter do
 
+  class Member < ActiveRecord::Base
+    has_many :pictures, as: :imageable, dependent: :destroy
+  end
+
   class TestAdapter
     include Support::AdapterHelper
     include PolymorphicConstraints::ConnectionAdapters::PostgreSQLAdapter
@@ -15,13 +19,23 @@ describe PolymorphicConstraints::ConnectionAdapters::PostgreSQLAdapter do
   it { is_expected.to respond_to(:remove_polymorphic_constraints) }
 
   describe 'add constraints' do
-    it 'returns expected add constraints sql' do
-      expect(subject.add_polymorphic_constraints(:imageable, :pictures)).to eql([upsert_triggers_sql, delete_triggers_sql])
-    end
+    context 'search strategy' do
+      it 'defaults to active_record_descendants search strategy' do
+        expect(subject.add_polymorphic_constraints(:imageable, :pictures)).to eql([upsert_triggers_sql_with_member,
+                                                                                   delete_triggers_sql_with_member])
+      end
 
-    it 'returns expected add constraints sql with polymorphic model options' do
-      expect(subject.add_polymorphic_constraints(:imageable, :pictures,
-                                                 polymorphic_models: [:employee, :product])).to eql([upsert_triggers_sql, delete_triggers_sql])
+      it 'returns expected add constraints with models_directory search strategy' do
+        expect(subject.add_polymorphic_constraints(:imageable, :pictures,
+                                                   search_strategy: :models_directory)).to eql([upsert_triggers_sql,
+                                                                                                delete_triggers_sql])
+      end
+
+      it 'returns expected add constraints sql with polymorphic model options' do
+        expect(subject.add_polymorphic_constraints(:imageable, :pictures,
+                                                   polymorphic_models: [:employee])).to eql([upsert_triggers_sql_only_employee,
+                                                                                             delete_triggers_sql_only_employee])
+      end
     end
   end
 
@@ -70,13 +84,15 @@ describe PolymorphicConstraints::ConnectionAdapters::PostgreSQLAdapter do
           IF TG_TABLE_NAME = ''employees'' AND EXISTS (
               SELECT id FROM pictures
               WHERE imageable_type = ''Employee'' AND imageable_id = OLD.id) THEN
-              RAISE EXCEPTION ''There are records in pictures that refer to the table % with id %. You must delete those records first.'', TG_TABLE_NAME, OLD.id;
+              RAISE EXCEPTION ''There are records in pictures that refer to the table % with id %.
+                You must delete those records first.'', TG_TABLE_NAME, OLD.id;
             RETURN NULL;
 
           ELSEIF TG_TABLE_NAME = ''products'' AND EXISTS (
               SELECT id FROM pictures
               WHERE imageable_type = ''Product'' AND imageable_id = OLD.id) THEN
-              RAISE EXCEPTION ''There are records in pictures that refer to the table % with id %. You must delete those records first.'', TG_TABLE_NAME, OLD.id;
+              RAISE EXCEPTION ''There are records in pictures that refer to the table % with id %.
+                You must delete those records first.'', TG_TABLE_NAME, OLD.id;
             RETURN NULL;
 
           ELSE
@@ -91,6 +107,138 @@ describe PolymorphicConstraints::ConnectionAdapters::PostgreSQLAdapter do
 
       CREATE TRIGGER check_products_delete_integrity_trigger
         BEFORE DELETE ON products
+        FOR EACH ROW EXECUTE PROCEDURE check_imageable_delete_integrity();
+    })
+  end
+
+  let(:upsert_triggers_sql_with_member) do
+    subject.strip_non_essential_spaces(%{
+      DROP FUNCTION IF EXISTS check_imageable_upsert_integrity()
+        CASCADE;
+
+      CREATE FUNCTION check_imageable_upsert_integrity() RETURNS TRIGGER AS '
+        BEGIN
+          IF NEW.imageable_type = ''Member'' AND EXISTS (
+              SELECT id FROM members WHERE id = NEW.imageable_id) THEN
+            RETURN NEW;
+
+          ELSEIF NEW.imageable_type = ''Employee'' AND EXISTS (
+              SELECT id FROM employees WHERE id = NEW.imageable_id) THEN
+            RETURN NEW;
+
+          ELSEIF NEW.imageable_type = ''Product'' AND EXISTS (
+              SELECT id FROM products WHERE id = NEW.imageable_id) THEN
+            RETURN NEW;
+
+          ELSE
+            RAISE EXCEPTION ''No % model with id %.'', NEW.imageable_type, NEW.imageable_id;
+            RETURN NULL;
+          END IF;
+        END'
+      LANGUAGE plpgsql;
+
+      CREATE TRIGGER check_imageable_upsert_integrity_trigger
+        BEFORE INSERT OR UPDATE ON pictures
+        FOR EACH ROW EXECUTE PROCEDURE check_imageable_upsert_integrity();
+    })
+  end
+
+  let(:delete_triggers_sql_with_member) do
+    subject.strip_non_essential_spaces(%{
+      DROP FUNCTION IF EXISTS check_imageable_delete_integrity()
+        CASCADE;
+
+      CREATE FUNCTION check_imageable_delete_integrity()
+      RETURNS TRIGGER AS '
+        BEGIN
+          IF TG_TABLE_NAME = ''members'' AND EXISTS (
+              SELECT id FROM pictures
+              WHERE imageable_type = ''Member'' AND imageable_id = OLD.id) THEN
+              RAISE EXCEPTION ''There are records in pictures that refer to the table % with id %.
+                You must delete those records first.'', TG_TABLE_NAME, OLD.id;
+            RETURN NULL;
+
+          ELSEIF TG_TABLE_NAME = ''employees'' AND EXISTS (
+              SELECT id FROM pictures
+              WHERE imageable_type = ''Employee'' AND imageable_id = OLD.id) THEN
+              RAISE EXCEPTION ''There are records in pictures that refer to the table % with id %.
+                You must delete those records first.'', TG_TABLE_NAME, OLD.id;
+            RETURN NULL;
+
+          ELSEIF TG_TABLE_NAME = ''products'' AND EXISTS (
+              SELECT id FROM pictures
+              WHERE imageable_type = ''Product'' AND imageable_id = OLD.id) THEN
+              RAISE EXCEPTION ''There are records in pictures that refer to the table % with id %.
+                You must delete those records first.'', TG_TABLE_NAME, OLD.id;
+            RETURN NULL;
+
+          ELSE
+            RETURN OLD;
+          END IF;
+        END'
+      LANGUAGE plpgsql;
+
+      CREATE TRIGGER check_members_delete_integrity_trigger
+        BEFORE DELETE ON members
+        FOR EACH ROW EXECUTE PROCEDURE check_imageable_delete_integrity();
+
+      CREATE TRIGGER check_employees_delete_integrity_trigger
+        BEFORE DELETE ON employees
+        FOR EACH ROW EXECUTE PROCEDURE check_imageable_delete_integrity();
+
+      CREATE TRIGGER check_products_delete_integrity_trigger
+        BEFORE DELETE ON products
+        FOR EACH ROW EXECUTE PROCEDURE check_imageable_delete_integrity();
+    })
+  end
+
+  let(:upsert_triggers_sql_only_employee) do
+    subject.strip_non_essential_spaces(%{
+      DROP FUNCTION IF EXISTS check_imageable_upsert_integrity()
+        CASCADE;
+
+      CREATE FUNCTION check_imageable_upsert_integrity() RETURNS TRIGGER AS '
+        BEGIN
+          IF NEW.imageable_type = ''Employee'' AND EXISTS (
+              SELECT id FROM employees WHERE id = NEW.imageable_id) THEN
+            RETURN NEW;
+
+          ELSE
+            RAISE EXCEPTION ''No % model with id %.'', NEW.imageable_type, NEW.imageable_id;
+            RETURN NULL;
+          END IF;
+        END'
+      LANGUAGE plpgsql;
+
+      CREATE TRIGGER check_imageable_upsert_integrity_trigger
+        BEFORE INSERT OR UPDATE ON pictures
+        FOR EACH ROW EXECUTE PROCEDURE check_imageable_upsert_integrity();
+    })
+  end
+
+  let(:delete_triggers_sql_only_employee) do
+    subject.strip_non_essential_spaces(%{
+      DROP FUNCTION IF EXISTS check_imageable_delete_integrity()
+        CASCADE;
+
+      CREATE FUNCTION check_imageable_delete_integrity()
+      RETURNS TRIGGER AS '
+        BEGIN
+          IF TG_TABLE_NAME = ''employees'' AND EXISTS (
+              SELECT id FROM pictures
+              WHERE imageable_type = ''Employee'' AND imageable_id = OLD.id) THEN
+              RAISE EXCEPTION ''There are records in pictures that refer to the table % with id %.
+                You must delete those records first.'', TG_TABLE_NAME, OLD.id;
+            RETURN NULL;
+
+          ELSE
+            RETURN OLD;
+          END IF;
+        END'
+      LANGUAGE plpgsql;
+
+      CREATE TRIGGER check_employees_delete_integrity_trigger
+        BEFORE DELETE ON employees
         FOR EACH ROW EXECUTE PROCEDURE check_imageable_delete_integrity();
     })
   end
