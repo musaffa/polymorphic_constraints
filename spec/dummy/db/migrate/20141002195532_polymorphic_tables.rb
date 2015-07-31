d5ab5ec77a7280a11cd0d4b979807d29130a3fbb
Ahmad Musaffa <musaffa_csemm@yahoo.com>
2015-07-31 17:09:18 +0600

    timestamps is set to null false

--- spec/dummy/db/migrate/20141002195532_polymorphic_tables.rb
+++ spec/dummy/db/migrate/20141002195532_polymorphic_tables.rb
@@ -1,16 +1,16 @@
 class PolymorphicTables < ActiveRecord::Migration
   def change
     create_table :employees do |t|
-      t.timestamps
+      t.timestamps null: false
     end
 
     create_table :products do |t|
-      t.timestamps
+      t.timestamps null: false
     end
 
     create_table :pictures do |t|
       t.references :imageable, polymorphic: true
-      t.timestamps
+      t.timestamps null: false
     end
 
     add_polymorphic_constraints :imageable, :pictures
